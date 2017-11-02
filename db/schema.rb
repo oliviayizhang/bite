40b633de74eedf1d8b9189301c8577a7a1b0a8c4
oliviayizhang <zhangyi.olivia@gmail.com>
2017-11-02 08:55:48 -0400

    added some styles to the home page and responsive header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029135234) do
+ActiveRecord::Schema.define(version: 20171101004831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171029135234) do
     t.bigint "group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["group_id"], name: "index_events_on_group_id"
   end
 
