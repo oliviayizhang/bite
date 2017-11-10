b01f628e4ffe2f5ec5de8f0d5b8d752848f2e1cb
oliviayizhang <zhangyi.olivia@gmail.com>
2017-11-10 00:07:16 -0500

    config action cable

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,6 +1,8 @@
-class ChatChannel < ApplicationCable::Channel
+class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    chatroom = Chatroom.find(params[:id])
+    stream_for chatroom
   end
 
   def unsubscribed
