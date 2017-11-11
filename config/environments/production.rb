5cc2429e74b976271d9d743c6c7a5351cf9b0f13
oliviayizhang <zhangyi.olivia@gmail.com>
2017-11-11 11:52:22 -0500

    config for action cable heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.web_socket_server_url = "wss://eetup.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://eetup.herokuapp.com', 'http://eetup.herokuapp.com']
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
